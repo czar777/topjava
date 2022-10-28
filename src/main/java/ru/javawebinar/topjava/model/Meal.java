6f3e2d2ee314e771b95dc69daf23c42fe7196935
Sergey Gerasimov <czar997@yandex.ru>
2022-10-28 11:43:49 +0300

    HW4.1 Meal Hibernate entity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,26 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+
+    @Column(name = "date_time")
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description")
+    @NotBlank
     private String description;
 
+    @Column(name = "calories")
+    @NotNull
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,67 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.slf4j.Logger;
 
 @Repository
 public class JpaMealRepository implements MealRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(JpaMealRepository.class);
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        User ref = em.getReference(User.class, userId);
+        meal.setUser(ref);
+        if (meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        } else {
+            return em.merge(meal);
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Query query = em.createQuery("SELECT m from Meal m WHERE m.id=:id AND m.user.id=:userId");
+        query.setParameter("id", id);
+        query.setParameter("userId", userId);
+        return (Meal) query.getSingleResult();
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        log.debug("getAll");
+        Query query = em.createQuery("SELECT m from Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC");
+        query.setParameter("userId", userId);
+        return query.getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        Query query = em.createQuery("SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime >=: startDateTime AND m.dateTime <: endDateTime ORDER BY m.dateTime DESC");
+        query.setParameter("userId", userId);
+        query.setParameter("endDateTime", endDateTime);
+        query.setParameter("startDateTime", startDateTime);
+        return query.getResultList();
     }
 }
