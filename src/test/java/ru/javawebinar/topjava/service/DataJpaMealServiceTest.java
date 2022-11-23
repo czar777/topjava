7e572dd2d337d1ca0dd55efe5c76cd50b98fadc9
Sergey Gerasimov <czar997@yandex.ru>
2022-11-23 17:54:30 +0300

    HW05 - 7 done

--- src/test/java/ru/javawebinar/topjava/service/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaMealServiceTest.java
@@ -1,7 +1,20 @@
 package ru.javawebinar.topjava.service;
 
+import org.hibernate.Hibernate;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
+import static ru.javawebinar.topjava.MealTestData.meal1;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ActiveProfiles({"datajpa", "hsqldb"})
 public class DataJpaMealServiceTest extends PMealServiceTest{
+
+    @Test
+    public void getMealWithUser() {
+        Meal meal = service.getMealWithUser(meal1.id(), USER_ID);
+        MEAL_MATCHER.assertMatch(meal, meal1);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
@@ -5,19 +5,17 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 
-import static ru.javawebinar.topjava.MealTestData.addUser;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles({"datajpa", "hsqldb"})
 public class DataJpaUserServiceTest extends PUserServiceTest {
 
-//    @Test
-//    public void getMealWithUser() {
-//        User user = service.getUserWithMeals(USER_ID);
-//        addUserMeals();
-//        addUser();
-//        USER_MATCHER.assertMatch(user, UserTestData.user);
-//
-//    }
+    @Test
+    public void getMealWithUser() {
+        User user = service.getUserWithMeals(USER_ID);
+        USER_MATCHER.assertMatch(user, UserTestData.user);
+        MEAL_MATCHER.assertMatch(meals, user.getMeals());
+    }
 
 }
