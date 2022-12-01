613faa68a08715b08f905c98b35558238347c7d5
Sergey Gerasimov <czar997@yandex.ru>
2022-12-01 13:39:56 +0300

    1.2

--- src/test/java/ru/javawebinar/topjava/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/ResourceControllerTest.java
@@ -4,17 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ResourceControllerTest extends AbstractControllerTest {
 
     @Test
     void testCss() throws Exception {
-
+        perform(MockMvcRequestBuilders.get("/resources/css/style.css"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/css;charset=UTF-8"));
     }
 }
